21909704823ba047268d75de0f200d93141f21cc
koko.le.gorille <koko.le.gorille@gmail.com>
2018-04-12 18:57:09 +0200

    Working MVP

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import NavBar from './components/nav_bar';
+
 const App = () => (
-  <div>
-    Hello from React!
+  <div className='container'>
+    <NavBar />
+    <h1>Hello from React!?</h1>
   </div>
 );
 
