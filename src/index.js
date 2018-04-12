8a6cfcf930f15acce29162bd00d1e3d7d1120fe7
koko.le.gorille <koko.le.gorille@gmail.com>
2018-04-12 18:10:11 +0200

    Add React

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-const world = 'world';
-console.log(`hello ${world}`);
+import React from 'react';
+import { render } from 'react-dom';
+import 'babel-polyfill';
+
+import './app.css';
+
+import App from './app.js'
+
+const app = document.getElementById('app');
+render(
+  <App />,
+  app,
+);
